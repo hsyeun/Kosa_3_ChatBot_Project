fb61a0a36fbeb837402ebe817b3a4aacafdf95e0
hsyeun <sooy0614@naver.com>
2022-02-10 14:21:37 +0900

    new ui

--- static/script.js
+++ static/script.js
@@ -1,130 +1,309 @@
-angular.module("swankyChat", [])
+var sendForm = document.querySelector('#chatform'),
+    textInput = document.querySelector('.chatbox'),
+    chatList = document.querySelector('.chatlist'),
+    userBubble = document.querySelectorAll('.userInput'),
+    botBubble = document.querySelectorAll('.bot__output'),
+    animateBotBubble = document.querySelectorAll('.bot__input--animation'),
+    overview = document.querySelector('.chatbot__overview'),
+    hasCorrectInput,
+    imgLoader = false,
+    animationCounter = 1,
+    animationBubbleDelay = 600,
+    input,
+    previousInput,
+    isReaction = false,
+    unkwnCommReaction = "I didn't quite get that.",
+    chatbotButton = document.querySelector(".submit-button")
 
-// Set Up Chat Controller
-.controller("chatCtrl", function($scope, $timeout, $rootScope) {
-  $scope.chats = [{
-    id: 0,
-    username: "Leela",
-    avatar: "https://imgflip.com/s/meme/Futurama-Leela.jpg",
-    messages: [
-      "I can explain. It's very valuable. You won't have time for sleeping, soldier, not with all the bed making you'll be doing",
-      "Who am I making this out to? We'll go deliver this crate like professionals, and then we'll go home",
-      "No! The cat shelter's on to me. I never loved you",
-      "Oh Leela! You're the only person I could turn to",
-      "Um, is this the boring, peaceful kind of taking to the streets",
-      "That's right, baby. I ain't your loverboy Flexo, the guy you love so much. You even love anyone pretending to be him!"
-    ]
-  }, {
-    id: 1,
-    username: "Bender",
-    avatar: "http://orig02.deviantart.net/9689/f/2012/027/9/c/mr_bender______classy__by_sgtconker1r-d4nqpzu.png",
-    messages: [
-      "Stop! Don't shoot fire stick in space canoe! Cause explosive decompression!",
-      "Fry! Stay back! He's too powerful! You guys aren't Santa!",
-      "Hi, I'm a naughty nurse, and I really need someone to talk to. $9.95 a minute",
-      "Who are you, my warranty?!",
-      "I will destroy you"
-    ]
-  }, {
-    id: 2,
-    username: "Fry",
-    avatar: "http://www.wallpaperno.com/thumbnails/detail/20121027/futurama%20fry%201920x1080%20wallpaper_www.wallpaperno.com_68.jpg",
-    messages: [
-      "Ooh, name it after me! But I've never been to the moon!",
-      "You don't know how to do any of those",
-      "The key to victory is discipline, and that means a well made bed",
-      "Stop bickering or I'm going to come back there and change your opinions manually",
-      "Can we have Bender Burgers again"
-    ]
-  }, {
-    id: 3,
-    username: 'Zoidberg',
-    avatar: "http://images2.fanpop.com/images/photos/3300000/Zoidberg-futurama-3305418-1024-768.jpg",
-    messages: [
-      "All I want is to be a monkey of moderate intelligence who wears a suit",
-      "Oh, I don't have time for this",
-      "No! The kind with looting and maybe starting a few fires!",
-      "Now, now. Perfectly symmetrical violence never solved anything",
-      "Dissect its brain"
-    ]
-  }];
-  
-  // Assign Pushed Messages To A User
-  $scope.text;
-  $scope.add = function() {
-    var vlu = $scope.value;
-    if($scope.text) {
-      $scope.chats[vlu].messages.push(this.text);
-      $scope.text = '';
-      console.log(vlu);
+sendForm.onkeydown = function(e){
+  if(e.keyCode == 13){
+    e.preventDefault();
+
+    //No mix ups with upper and lowercases
+    var input = textInput.value.toLowerCase();
+
+    //Empty textarea fix
+    if(input.length > 0) {
+      createBubble(input)
     }
   }
+};
 
-  // Setting The Value Scope Equal To The Chat.id Which Is Retrieved Via Ng Click - We Pass The Chat.id Through The Function
-  $scope.value;
-  $scope.uid = function(ix) {
-    console.log(ix);
+sendForm.addEventListener('submit', function(e) {
+  //so form doesnt submit page (no page refresh)
+  e.preventDefault();
 
-    function ixy() {
-      $rootScope.value = ix;
+  //No mix ups with upper and lowercases
+  var input = textInput.value.toLowerCase();
+
+  //Empty textarea fix
+  if(input.length > 0) {
+    createBubble(input)
+  }
+}) //end of eventlistener
+
+var createBubble = function(input) {
+  //create input bubble
+  var chatBubble = document.createElement('li');
+  chatBubble.classList.add('userInput');
+
+  //adds input of textarea to chatbubble list item
+  chatBubble.innerHTML = input;
+
+  //adds chatBubble to chatlist
+  chatList.appendChild(chatBubble)
+
+  checkInput(input);
+}
+
+var checkInput = function(input) {
+  hasCorrectInput = false;
+  isReaction = false;
+  //Checks all text values in possibleInput
+  for(var textVal in possibleInput){
+    //If user reacts with "yes" and the previous input was in textVal
+    if(input == "yes" || input.indexOf("yes") >= 0){
+      if(previousInput == textVal) {
+        console.log("sausigheid");
+
+        isReaction = true;
+        hasCorrectInput = true;
+        botResponse(textVal);
+      }
     }
-    // Delay Our Scope Change To Create A Smoother Transition
-    $timeout(ixy, 750);
+    if(input == "no" && previousInput == textVal){
+      unkwnCommReaction = "For a list of commands type: Commands";
+      unknownCommand("I'm sorry to hear that :(")
+      unknownCommand(unkwnCommReaction);
+      hasCorrectInput = true;
+    }
+    //Is a word of the input also in possibleInput object?
+    if(input == textVal || input.indexOf(textVal) >=0 && isReaction == false){
+			console.log("succes");
+      hasCorrectInput = true;
+      botResponse(textVal);
+		}
+	}
+  //When input is not in possibleInput
+  if(hasCorrectInput == false){
+    console.log("failed");
+    unknownCommand(unkwnCommReaction);
+    hasCorrectInput = true;
   }
+}
 
-});
+// debugger;
 
-// Animation Styles
-$(function() {
+function botResponse(textVal) {
+  //sets previous input to that what was called
+  // previousInput = input;
 
-  var index = 0;
-  
-  function initScroll() {
-    $(".message-wrap").animate({ 
-      scrollTop: $("main").height() 
-    }, 1000);
-  }
-  
-  function scroll() {
-    $(".message-wrap").animate({
-      scrollTop: 9000
-    }, 1000);
-  }
-  
-  $("input[type='submit']").click(function() {
-    scroll();
-  });
+  //create response bubble
+  var userBubble = document.createElement('li');
+  userBubble.classList.add('bot__output');
 
-  $("aside").find("li").click(function() {
-    initScroll();
-    $(".init").animate({
-      'opacity': '0'
-    }, 500);
-  });
-
-  $("aside").find("li").click(function() {
-    if (index == 1) {
-      index = 0;
-      $(".message-wrap").find(".message").css({
-        'opacity': '1'
-      });
+  if(isReaction == true){
+    if (typeof reactionInput[textVal] === "function") {
+    //adds input of textarea to chatbubble list item
+      userBubble.innerHTML = reactionInput[textVal]();
     } else {
-      index = 0;
-      $(".message-wrap").find(".message").css({
-        'opacity': '0'
-      });
-      $(".loader").delay(500).animate({
-        'opacity': '1'
-      });
-      setTimeout(function() {
-        index = 0;
-        $(".message-wrap").find(".message").css({
-          'opacity': '1'
-        });
-        $(".loader").animate({
-          'opacity': '0'
-        });
-      }, 3000)
+      userBubble.innerHTML = reactionInput[textVal];
     }
-  });
-});
+  }
+
+  if(isReaction == false){
+    //Is the command a function?
+    if (typeof possibleInput[textVal] === "function") {
+      // console.log(possibleInput[textVal] +" is a function");
+    //adds input of textarea to chatbubble list item
+      userBubble.innerHTML = possibleInput[textVal]();
+    } else {
+      userBubble.innerHTML = possibleInput[textVal];
+    }
+  }
+  //add list item to chatlist
+  chatList.appendChild(userBubble) //adds chatBubble to chatlist
+
+  // reset text area input
+  textInput.value = "";
+}
+
+function unknownCommand(unkwnCommReaction) {
+  // animationCounter = 1;
+
+  //create response bubble
+  var failedResponse = document.createElement('li');
+
+  failedResponse.classList.add('bot__output');
+  failedResponse.classList.add('bot__output--failed');
+
+  //Add text to failedResponse
+  failedResponse.innerHTML = unkwnCommReaction; //adds input of textarea to chatbubble list item
+
+  //add list item to chatlist
+  chatList.appendChild(failedResponse) //adds chatBubble to chatlist
+
+  animateBotOutput();
+
+  // reset text area input
+  textInput.value = "";
+
+  //Sets chatlist scroll to bottom
+  chatList.scrollTop = chatList.scrollHeight;
+
+  animationCounter = 1;
+}
+
+function responseText(e) {
+
+  var response = document.createElement('li');
+
+  response.classList.add('bot__output');
+
+  //Adds whatever is given to responseText() to response bubble
+  response.innerHTML = e;
+
+  chatList.appendChild(response);
+
+  animateBotOutput();
+
+  console.log(response.clientHeight);
+
+  //Sets chatlist scroll to bottom
+  setTimeout(function(){
+    chatList.scrollTop = chatList.scrollHeight;
+    console.log(response.clientHeight);
+  }, 0)
+}
+
+function responseImg(e) {
+  var image = new Image();
+
+  image.classList.add('bot__output');
+  //Custom class for styling
+  image.classList.add('bot__outputImage');
+  //Gets the image
+  image.src = "/images/"+e;
+  chatList.appendChild(image);
+
+  animateBotOutput()
+  if(image.completed) {
+    chatList.scrollTop = chatList.scrollTop + image.scrollHeight;
+  }
+  else {
+    image.addEventListener('load', function(){
+      chatList.scrollTop = chatList.scrollTop + image.scrollHeight;
+    })
+  }
+}
+
+//change to SCSS loop
+function animateBotOutput() {
+  chatList.lastElementChild.style.animationDelay= (animationCounter * animationBubbleDelay)+"ms";
+  animationCounter++;
+  chatList.lastElementChild.style.animationPlayState = "running";
+}
+
+function commandReset(e){
+  animationCounter = 1;
+  previousInput = Object.keys(possibleInput)[e];
+}
+
+// hlep
+
+var possibleInput = {
+  // "hlep" : this.help(),
+  "help" : function(){
+    responseText("You can type a command in the chatbox")
+    responseText("Something like &quot;Navvy, please show me Mees&rsquo; best work&quot;")
+    responseText("Did you find a bug or problem? Tweet me @MeesRttn")
+    commandReset(0);
+    return
+    },
+  "best work" : function(){
+    responseText("I will show you Mees' best work!");
+    responseText("These are his <a href='#animation'>best animations</a>")
+    responseText("These are his <a href='#projects'>best projects</a>")
+    responseText("Would you like to see how I was built? (Yes/No)")
+    commandReset(1);
+    return
+    },
+  "about" : function(){
+    responseText("This is me, Navvy's maker, Mees Rutten");
+    responseText("I'm a 22 year old Communication and Multimedia Design student");
+    responseText("My ambition is to become a great Creative Front-End Developer");
+    responseText("Would you like to know about Mees' vision? (Yes/No)");
+    commandReset(2);
+    return
+    },
+  "experience" : function(){
+    responseText("Mees has previously worked at:");
+    responseText("Cobra Systems as web- developer / designer");
+    responseText("BIT Students as web- developer / designer");
+    responseText("MediaMonks as a junior Front-end Developer");
+    commandReset(3);
+    return
+  },
+  "hobbies" : function(){
+    responseText("Mees loves:");
+    responseText("Coding complicated chatbots");
+    responseText("Family time");
+    responseText("Going out with friends");
+    responseText("Working out");
+    commandReset(4);
+    return
+  },
+  "interests" : function(){
+    responseText("Mees loves:");
+    responseText("Coding complicated chatbots");
+    responseText("Family time");
+    responseText("Going out with friends");
+    responseText("Working out");
+    commandReset(5);
+    return
+  },
+  "vision" : function(){
+    responseText("Things I want to learn or do:");
+    responseText("Get great at CSS & JS animation");
+    responseText("Create 3D browser experiences");
+    responseText("Learn Three.js and WebGL");
+    responseText("Combine Motion Design with Front-End");
+    commandReset(6);
+    return
+  },
+  "contact" : function(){
+    responseText("email: <a href='mailto:meesrutten@gmail.com?Subject=Hello%20Mees' target='_top'>send me a message</a>");
+    responseText("Twitter: <a href='https://twitter.com/meesrttn'>@MeesRttn</a>");
+    commandReset(7);
+    return
+  },
+  "commands" : function(){
+    responseText("This is a list of commands Navvy knows:")
+    responseText("help, best work, about, vision, experience, hobbies / interests, contact, rick roll");
+    commandReset(8);
+    return
+  },
+  "rick roll" : function(){
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    },
+  // work experience
+}
+
+var reactionInput = {
+  "best work" : function(){
+    //Redirects you to a different page after 3 secs
+    responseText("On this GitHub page you'll find everything about Navvy");
+    responseText("<a href='https://github.com/meesrutten/chatbot'>Navvy on GitHub</a>")
+    animationCounter = 1;
+    return
+  },
+  "about" : function(){
+    responseText("Things I want to learn or do:");
+    responseText("Get great at CSS & JS animation");
+    responseText("Create 3D browser experiences");
+    responseText("Learn Three.js and WebGL");
+    responseText("Combine Motion Design with Front-End");
+    animationCounter = 1;
+    return
+    }
+}
