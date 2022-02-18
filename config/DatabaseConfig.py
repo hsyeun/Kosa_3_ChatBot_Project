aea0f2b444ce73611e8b7b59227a86b5436153a2
Jenny0122 <asj4459@naver.com>
2022-02-18 10:23:57 +0900

    삭제

--- config/DatabaseConfig.py
+++
@@ -1,7 +0,0 @@
-DB_HOST = "127.0.0.1"
-DB_USER = "homestead"
-DB_PASSWORD = "secret"
-DB_NAME = "homestead"
-
-def DatabaseConfig():
-    global DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
--- movie_crowl/word2vec모델test.py
+++
@@ -1,5 +0,0 @@
-from gensim.models import Word2Vec
-
-model = Word2Vec.load('nvmc.model')
-
-print(model.wv.most_similar('영화관',topn=3))
