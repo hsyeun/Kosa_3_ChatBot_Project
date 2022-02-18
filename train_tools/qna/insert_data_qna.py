456757adc99345dc81db6ee09b64e6867d3dcf76
hsyeun <sooy0614@naver.com>
2022-02-18 14:51:35 +0900

    mariaDB 데이터생성 완료

--- train_tools/qna/insert_data_qna.py
+++ train_tools/qna/insert_data_qna.py
@@ -8,14 +8,14 @@ from Config.DatabaseConfig import * # DB 접속 정보 불러오기
 def all_clear_train_data(db):
     # 기존 학습 데이터 삭제
     sql = '''
-            delete from chatbot_train_data
+            delete from chatbot_qna_data
         '''
     with db.cursor() as cursor:
         cursor.execute(sql)
 
     # auto increment 초기화
     sql = '''
-    ALTER TABLE chatbot_train_data AUTO_INCREMENT=1
+    ALTER TABLE chatbot_qna_data AUTO_INCREMENT=1
     '''
     with db.cursor() as cursor:
         cursor.execute(sql)
@@ -23,25 +23,25 @@ def all_clear_train_data(db):
 
 # db에 데이터 저장
 def insert_data(db, xls_row):
-    intent, ner, query, answer, answer_img_url = xls_row
+    intent, ner, query, answer = xls_row
 
     sql = '''
-        INSERT chatbot_train_data(intent, ner, query, answer) 
+        INSERT chatbot_qna_data(intent, ner, query, answer) 
         values(
          '%s', '%s', '%s', '%s'
         )
-    ''' % (intent.value, ner.value, query.value, answer.value, answer_img_url.value)
+    ''' % (intent.value, ner.value, query.value, answer.value)
 
     # 엑셀에서 불러온 cell에 데이터가 없는 경우, null 로 치환
     sql = sql.replace("'None'", "null")
 
     with db.cursor() as cursor:
         cursor.execute(sql)
-        print('{} 저장'.format(query.value))
+        # print('{} 저장'.format(query.value))
         db.commit()
 
 
-train_file = 'C:/class/chatbot-master/train_tools\qna/t_data.xlsx'
+train_file = r'C:\dev\Project_chatbot\Kosa_3_ChatBot_Project\train_tools\qna\DB_QnA.xlsx'
 db = None
 try:
     db = pymysql.connect(
@@ -55,10 +55,15 @@ try:
     # 기존 학습 데이터 초기화
     all_clear_train_data(db)
 
+    # df = pd.read_excel(train_file)
+    # print(df.info())
+
     # 학습 엑셀 파일 불러오기
     wb = openpyxl.load_workbook(train_file)
+    ws = wb.sheetnames
+    print(ws)
     sheet = wb['Sheet1']
-    for row in sheet.iter_rows(min_row=2): # 해더는 불러오지 않음
+    for row in sheet.iter_rows(min_row=2, min_col=2): # 해더는 불러오지 않음
         # 데이터 저장
         insert_data(db, row)
 
