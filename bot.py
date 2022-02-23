ad4c6a18217d66725521d612cfc45922c2646c14
a76xre <58390751+a76xre@users.noreply.github.com>
2022-02-23 16:00:45 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ def to_client(conn, addr, params):
             elif intent_name == '영화관문의':
                 thr_name = [s for s in ner_predicts if 'B_TER' in s][0][0]
                 thr_info = f.find_thr_info(thr_name)
-                answer = '영화관이름 : '+thr_info[0] + '\n주소 : '+thr_info[1] + '\n전화번호 : '+thr_info[2]
+                answer = '영화관이름 : ' + thr_info[0] + '\n주소 : ' + thr_info[1] + '\n전화번호 : '+thr_info[2]
 
         except:
             answer = "죄송해요 무슨 말인지 모르겠어요. 조금 더 공부 할게요."
